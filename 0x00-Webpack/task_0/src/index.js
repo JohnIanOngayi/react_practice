0932d2df8ca3fd3c2b22eaab56e728fedbd61085
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-08 14:29:45 +0300

    Task 0
    - used default webpack configurations

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import $ from "jquery";
+
+const para_1 = $("<p></p>").text("ALX Dashboard");
+const para_2 = $("<p></p>").text("Dashboard data for the students");
+const para_3 = $("<p></p>").text("Copyright - ALX");
+
+$(document).ready(function () {
+  $("body").append(para_1);
+  $("body").append(para_2);
+  $("body").append(para_3);
+});
