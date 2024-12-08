37abb35735512d8d697eb1255b59d58af52314ce
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-08 16:24:34 +0300

    Task 1 Boilerplate

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import $ from "jquery";
+
+$(document).ready(function () {
+  const para_1 = $("<p></p>").text("ALX Dashboard");
+  const para_2 = $("<p></p>").text("Dashboard data for the students");
+  const btn = $("<button></button>").text("Click here to get started");
+  const para_3 = $("<p></p>").attr("id", "count");
+  const para_4 = $("<p></p>").text("Copyright - ALX");
+});
