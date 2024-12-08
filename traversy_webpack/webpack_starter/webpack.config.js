52ee3b2e1652b197b331c31b8a5d6102fbd996b3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-08 11:25:27 +0300

    Added Babel, source-map, BundleAnalyzerPlugin

--- traversy_webpack/webpack_starter/webpack.config.js
+++ traversy_webpack/webpack_starter/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const BundleanalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -10,7 +13,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
     clean: true, // Keep only latest file
+    assetModuleFilename: "[name][ext]",
   },
+  devtool: "source-map", // Map debugs in output file
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
@@ -27,6 +32,20 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
@@ -35,5 +54,6 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
