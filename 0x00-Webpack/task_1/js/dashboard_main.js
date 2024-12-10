3159be698770b2e5e5d19561769b2951597f59d5
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-10 13:41:35 +0300

    Task 1 Added bundle.js and HTML file
    - Used htm-weback-plugin for index.html

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import { debounce } from "lodash";
 
 $(document).ready(function () {
   const para_1 = $("<p></p>").text("ALX Dashboard");
@@ -6,4 +7,26 @@ $(document).ready(function () {
   const btn = $("<button></button>").text("Click here to get started");
   const para_3 = $("<p></p>").attr("id", "count");
   const para_4 = $("<p></p>").text("Copyright - ALX");
+  let count = 0;
+
+  $("body").append(para_1);
+  $("body").append(para_2);
+  $("body").append(btn);
+  $("body").append(para_3);
+  $("body").append(para_4);
+
+  const paraCount = $("p#count");
+  const updateBtn = $("button");
+  function updateCounter(text) {
+    paraCount.text(text);
+  }
+
+  updateBtn.on(
+    "click",
+    debounce(() => {
+      console.log("clicked");
+      count++;
+      updateCounter(count);
+    }, 250),
+  );
 });
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  watch: true,
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Task 1 Webpack App",
+      filename: "index.html",
+    }),
+  ],
+};
