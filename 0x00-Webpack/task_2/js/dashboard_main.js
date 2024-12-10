a8790907b181f6bf7bf83cf1bdf3d57e036d51c0
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-10 14:39:13 +0300

    Task 2: Added asset resolutions and css styling

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+import $ from "jquery";
+import { debounce } from "lodash";
+import "../css/main.css";
+
+$(document).ready(function () {
+  const para_1 = $("<p></p>").text("ALX Dashboard");
+  const para_2 = $("<p></p>").text("Dashboard data for the students");
+  const btn = $("<button></button>").text("Click here to get started");
+  const para_3 = $("<p></p>").attr("id", "count");
+  const para_4 = $("<p></p>").text("Copyright - ALX");
+
+  const para_5 = $("<div></div>").attr("id", "logo");
+
+  let count = 0;
+
+  $("body").append(para_5);
+  $("body").append(para_1);
+  $("body").append(para_2);
+  $("body").append(btn);
+  $("body").append(para_3);
+  $("body").append(para_4);
+
+  const paraCount = $("p#count");
+  const updateBtn = $("button");
+  function updateCounter(text) {
+    paraCount.text(text);
+  }
+
+  updateBtn.on(
+    "click",
+    debounce(() => {
+      console.log("clicked");
+      count++;
+      updateCounter(count);
+    }, 250),
+  );
+});
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,1 +1,49 @@
-#!/usr/bin/node
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  watch: true,
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      // css and styling directives
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      // Image directives
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Task 2 Webpack App",
+      filename: "index.html",
+    }),
+  ],
+};
