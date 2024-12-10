a6887d60db4125d59ab39b1dac7c3359672638a7
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-10 16:00:06 +0300

    Task 2: Corrected output dir
    - Changed from ./dist to ./public

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, "public"),
     },
     port: 3000,
     open: true,
