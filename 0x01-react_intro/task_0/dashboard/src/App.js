f68af4f1974c3232140949c7b94e70ac80006660
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-15 13:30:39 +0300

    Task 0 - Basic create-react-app

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+import logo from "./175b85183ecedb529e14.jpg";
+import icon from "./e240f8157634d33a9757.ico";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </header>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div class="App-footer">
+        <p>Copyright 2020 - ALX</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
